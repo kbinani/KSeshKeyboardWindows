7759c1e6a532a8e76681c87f94f764f8eb7b30e5
kbinani <kbinani.bt@gmail.com>
2026-02-16 08:25:00 +0900

    feat: Change output directory

--- script/dev_install_as_root.bat
+++ script/dev_install_as_root.bat
@@ -1,5 +1,5 @@
 regsvr32 /u "C:\Windows\System32\KSeshIME.dll"
-copy "x64\Debug\KSeshIME.dll" "C:\Windows\System32\"
+copy "build\x64\Debug\KSeshIME.dll" "C:\Windows\System32\"
 icacls "C:\Windows\System32\KSeshIME.dll" /grant "ALL APPLICATION PACKAGES":(RX)
 regsvr32 "C:\Windows\System32\KSeshIME.dll"
 taskkill /F /IM ctfmon.exe
